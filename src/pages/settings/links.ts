f64aca8e6f7250393a706181791157ef45724fac
Maximillian <maxsilvauk@gmail.com>
2020-06-12 14:43:00 +0100

    Delete all settings pages

--- src/pages/settings/links.ts
+++
@@ -1,9 +0,0 @@
-import { SideNavLinks } from '~store/sidenav/types';
-
-export const settingsPageLinks: SideNavLinks[] = [{
-  name: 'Api Keys',
-  path: '/settings/api-keys'
-}, {
-  name: 'Credentials',
-  path: '/settings/credentials'
-}];
